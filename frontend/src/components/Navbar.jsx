6a756a20a7d4983b8636dde48b2c9cf8111705c8
Alpha <ajayvish21099@gmail.com>
2024-01-14 02:47:59 +0530

    x

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -49,21 +49,21 @@ const Navbar = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a  href='/'>Home</a></li>
+        <li><Link  to='/'>Home</Link></li>
         
-        <li><a href='/hacker'>Hacker List</a></li>
+        <li><Link to='/hacker'>Hacker List</Link></li>
         
-        <li><a href='/contact'>About</a></li>
+        <li><Link to='/contact'>About</Link></li>
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">CodeHacker</a>
+    <Link to='/' className="btn btn-ghost text-xl">CodeHacker</Link>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
       <li><a href='/'>Home</a></li>
    
       <li><Link to='/hacker'>Hacker List</Link></li>
-      <li><a href='/contact'>About</a></li>
+      <li><Link to='/contact'>About</Link></li>
     </ul>
   </div>
   <div className="navbar-end">
